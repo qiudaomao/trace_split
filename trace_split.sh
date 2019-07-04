839301ceeba3ef7bfa9ed1d1ceebec11156d70b5
qiudaomao <qiudaomao@gmail.com>
2019-07-04 15:48:23 +0800

    improve performance

--- trace_split.sh
+++ trace_split.sh
@@ -11,7 +11,7 @@ if [ ! -f $input_file ]; then
     exit 0
 fi
 
-header_end=$(grep -n '# *| *| *| *| *| *| *| *| *|' ${input_file} | sed 's/:.*//g')
+header_end=$(grep -n '# *| *| *| *| *| *| *| *| *|' ${input_file} | head -n 1 | sed 's/:.*//g')
 echo "header_end: ${header_end}"
 echo "sed -n '1,${header_end}p' > header.html"
 sed -n "1,${header_end}p" ${input_file} > header.html
@@ -19,6 +19,7 @@ tail -n 4 ${input_file} > footer.html
 
 total_lines=$(wc -l ${input_file} | awk '{print $1}')
 total_lines=$((total_lines-header_end-4))
+mtotal_lines=$((total_lines+header_end))
 echo "total: total_lines: $total_lines"
 
 part_num=$(((total_lines+LINE-1)/LINE))
@@ -28,7 +29,6 @@ i=0
 while [ $i -lt $part_num ]; do
     start=$((header_end+LINE*i+1))
     end=$((header_end+LINE*(i+1)))
-    mtotal_lines=$((total_lines+header_end))
     if [ $end -gt $mtotal_lines ]; then
         end=$mtotal_lines
     fi
